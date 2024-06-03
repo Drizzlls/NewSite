0d925997cf7ae3a12c9363a26cca6ddbefc5955b
DrizzllDev <drizzllsvery@yandex.ru>
2024-06-03 15:54:47 +0300

    сделал еще несколько страниц

--- siteRazdorov/sitePages/urls.py
+++ siteRazdorov/sitePages/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth import views, urls
-from .views import IndexPage, AboutPage, NewsPage,ContactsPage
+from .views import IndexPage, AboutPage, NewsPage,ContactsPage, CooperationPage, ReviewsPage, CasesPage, FAQPage, CalcPage, SalePage
 
 urlpatterns = [
     path('', IndexPage.as_view(), name='index'),
     path('about/', AboutPage.as_view(), name='about'),
     path('news/', NewsPage.as_view(), name='news'),
     path('contacts/', ContactsPage.as_view(), name='contacts'),
+    path('cooperation/', CooperationPage.as_view(), name='cooperation'),
+    path('reviews/', ReviewsPage.as_view(), name='reviews'),
+    path('cases/', CasesPage.as_view(), name='cases'),
+    path('faq/', FAQPage.as_view(), name='faq'),
+    path('calculator/', CalcPage.as_view(), name='calculator'),
+    path('sale/', SalePage.as_view(), name='sale'),
 
 ]
--- siteRazdorov/sitePages/views.py
+++ siteRazdorov/sitePages/views.py
@@ -4,9 +4,13 @@ from django.views import View
 class IndexPage(View):
     """ Главная страница """
     template_name = 'pages/index.html'
+    context = {
+        'title' : 'Заполненный Title',
+        'description' : 'Описание страницы'
+    }
 
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name, context=self.context)
 
 
 class AboutPage(View):
@@ -26,6 +30,49 @@ class NewsPage(View):
 class ContactsPage(View):
     """ Страница 'Новости' """
     template_name = 'pages/contacts.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class CooperationPage(View):
+    """ Страница 'Сотрудничество' """
+    template_name = 'pages/cooperation.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class ReviewsPage(View):
+    """ Страница 'Отзывы клиентов' """
+    template_name = 'pages/reviews.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class CasesPage(View):
+    """ Страница 'Завершенные дела' """
+    template_name = 'pages/cases.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class FAQPage(View):
+    """ Страница 'Основные вопросы' """
+    template_name = 'pages/faq.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class CalcPage(View):
+    """ Страница 'Калькулятор стоимости банкротства' """
+    template_name = 'pages/calc.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class SalePage(View):
+    """ Страница 'Скидки' """
+    template_name = 'pages/sale.html'
 
     def get(self, request):
         return render(request, self.template_name)
