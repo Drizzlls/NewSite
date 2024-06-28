3c88a6ddc810fbcf0076985eb15b8bc5f36c9d16
Drizzlls <drizzlls@yandex.ru>
2024-06-28 16:26:16 +0300

    отредактировал сообщение в тг

--- siteRazdorov/siteRazdorov/customLogging.py
+++ siteRazdorov/siteRazdorov/customLogging.py
@@ -5,7 +5,7 @@ import requests
 class TelegramBotHandler(logging.Handler):
 
     def emit(self, record):
-        text = f'Время — {record.asctime}\nТип проблемы — {record.levelname}\nФункция — {record.funcName}({record.lineno})\nПроблема — {record.message}'
+        text = f'Время — {record.asctime}\nТип проблемы — {record.levelname}\nФункция — {record.filename}/{record.funcName}({record.lineno})\nПроблема — {record.message}'
         req = requests.get(f'https://api.telegram.org/bot7354303530:AAFXOhnoXVHLZZ038lH-pM9WZO9MosuKJeU/sendMessage?chat_id=272635960&text={text}')
 
 
